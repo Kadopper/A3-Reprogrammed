dea003ff3a2f80fdcf5b2051f3ba0e346ca575af
Kadopper <christofferditlev@gmail.com>
2024-10-16 10:45:01 +0200

    2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
+<template>
+  <div>
+    <button @click="openMenu1">Open menu 1</button>
+    <div v-if="menu1" class="menu1">
+      
+    </div>
+  </div>
+</template>
+
 <script setup>
+  import { ref } from 'vue';
+  
+  const menu1IsOpen = ref(false)
+
+  function openMenu1() {
+    menu1IsOpen = !menu1IsOpen
+  }
 
 </script>
 
-<template>
-
-</template>
 
 <style scoped>
+.menu1{
+  width: 100px;
+  height: 100px;
+  background-color: red;
+  position: relative;
+}
 
 </style>
