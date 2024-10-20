21f07a5c66f7870df1917bd8c278064e10989a54
Kadopper <christofferditlev@gmail.com>
2024-10-20 22:28:00 +0200

    Version 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
   
-
   <div>
-    <div
-      class="buttonHome" @click="openMenu1">
-    </div>
-    <div
-      class="buttonSearch" @click="openMenu1">
-    </div>
-    <div
-      class="buttonTicket" @click="openMenu1">
-    </div>
-    <div
-      class="buttonProfile" @click="openMenu1">
-    </div>
-      <img v-if="menuHome" src="/src/assets/homeMenu.svg">
-      <img v-if="menuSearch" src="/src/assets/menuSearch.svg">
-      <img v-if="menuTicket" src="/src/assets/billetMenu.svg">
-      <img v-if="menuProfile" src="/src/assets/ticketMenu.svg">
+    <div class="buttonHome" @click="openMenu('home')"></div>
+    <div class="buttonSearch" @click="openMenu('search')"></div>
+    <div class="buttonTicket" @click="openMenu('ticket')"></div>
+    <div class="buttonProfile" @click="openMenu('profile')"></div>
+
+    <img v-if="activeMenu === 'home'" src="/src/assets/homeMenu.svg">
+    <img v-if="activeMenu === 'search'" src="/src/assets/searchMenu.svg">
+    <img v-if="activeMenu === 'ticket'" src="/src/assets/ticketMenu.svg">
+    <img v-if="activeMenu === 'profile'" src="/src/assets/profileMenu.svg">
   </div>
   
 </template>
@@ -25,14 +17,13 @@
 <script setup>
   import { ref } from 'vue';
   
-  // fix
-  //let menu =  ["home","seach","ticket","profile"]
-  const menu1IsOpen = ref(false)
+// hold current menu
+  const activeMenu = ref('');
 
-  function openMenu1() {
-    menu1IsOpen.value = !menu1IsOpen.value
-  }
-
+// change menu by using the click input menu function 
+  function openMenu(menu) {
+    activeMenu.value = menu;
+}
 </script>
 
 
@@ -45,44 +36,48 @@
 }
 
 .buttonHome{
-  border-color: red;
-  border-style: dashed;
-  top: 782px;
+  /* border-color: red;
+  border-style: dashed; */ 
+  top: 780px;
   left: 36px;
   width: 45px;
   height: 45px;
   position: absolute;
+  cursor: pointer;
 }
 
 .buttonSearch{
-  border-color: red;
-  border-style: dashed;
-  top: 782px;
+  /* border-color: red;
+  border-style: dashed; */
+  top: 780px;
   left: 131px;
   width: 45px;
   height: 45px;
   position: absolute;
+  cursor: pointer;
 }
 
 
 .buttonTicket{
-  border-color: red;
-  border-style: dashed;
-  top: 782px;
-  left: 227px;
+  /* border-color: red;
+  border-style: dashed; */ 
+  top: 780px;
+  left: 225px;
   width: 45px;
   height: 45px;
   position: absolute;
+  cursor: pointer;
 }
 
 .buttonProfile{
-  border-color: red;
-  border-style: dashed;
-  top: 782px;
-  left: 322px;
+  /* border-color: red;
+  border-style: dashed; */
+  top: 780px;
+  left: 319px;
   width: 45px;
   height: 45px;
   position: absolute;
+  cursor: pointer;
 }
 
 
